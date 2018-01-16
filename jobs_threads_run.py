840020ac48c7dda593a4dff02d1af4c19686e945
ShengHe <fuyawangye@163.com>
2018-01-16 22:40:26 +0800

    add error catch

--- jobs_threads_run.py
+++ jobs_threads_run.py
@@ -17,6 +17,8 @@ class JobsThreadsRun(object):
             raise ValueError("func 必须是一个函数对象用于执行job: {}".format(type(func)))
         self.jobs = jobs
         self.unfinish_jobs = copy.deepcopy(self.jobs)
+        self.error_jobs = []
+        self.success_jobs = []
         self.run_func = func
         self.thread_num = num_threads
         self.__lock = threading.Lock()
@@ -25,13 +27,13 @@ class JobsThreadsRun(object):
     def start(self):
         for i in self._get_threads():
             i.start()
-    
+
     def _get_threads(self):
         for i in range(self.thread_num):
             temp = Thread(target=self._run_threads)
             self._current_threads.append(temp)
         return self._current_threads
-    
+
     def _run_threads(self):
         while True:
             self.__lock.acquire()
@@ -40,5 +42,8 @@ class JobsThreadsRun(object):
                 break
             job = self.unfinish_jobs.pop()
             self.__lock.release()
-            self.run_func(job)
-            
+            try:
+                self.run_func(job)
+                self.success_jobs.append(job)
+            except Exception as e:
+                self.error_jobs.append((job, e))
--- test.py
+++ test.py
@@ -5,44 +5,20 @@ import time
 from threading import Thread
 
 
-lock = threading.Lock()
-jobs = [1,2,3,4,5,6,7]  # 任务
-
 # 全局配置文件
 config = {
     "THREAD_NUM": 3,  # 线程数
     "LOG_FILE": "exec.log"  # 日志路径
 }
 
-
 def run_job(job):
     time.sleep(3)
     print("run_job:" + str(job))
 
-# def run_jobs(lock):
-#     while True:
-#         lock.acquire()
-#         if len(jobs) == 0:
-#             lock.release()
-#             break
-#         job = jobs.pop()
-#         lock.release()
-#         try:
-#             run_job(job)
-#         except Exception as e:
-#             print('执行任务: {0} 出错'.format(job))
-
-# def get_threads(num, func, lock):
-#     for i in range(num):
-#         yield Thread(target=func, args=(lock, ))
-
-# def main():
-#     for i in get_threads(config['THREAD_NUM'], run_jobs, lock):
-#         i.start()
 from jobs_threads_run import JobsThreadsRun
 
 def main():
-    run = JobsThreadsRun([1,2,3,4,5,56,6], run_job, 5)
+    run = JobsThreadsRun([1,2,3,4,5,56,6], run_job, 2)
     run.start()
 
 if __name__ == "__main__":
